811c76ac90db807e2ee64dca4c5be73a0a1e067b
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:35:24 +0700

    Add build in UserDetailsImpl 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserDetailsImpl.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserDetailsImpl.java
@@ -11,6 +11,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
+import java.util.Objects;
 import java.util.stream.Collectors;
 
 /**
@@ -57,5 +58,35 @@ public class UserDetailsImpl implements UserDetails {
                 user.getMobile(),
                 user.getDrivingLisence(),
                 authorities);
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o)
+            return true;
+        if (o == null || getClass() != o.getClass())
+            return false;
+        UserDetailsImpl user = (UserDetailsImpl) o;
+        return Objects.equals(id, user.id);
     }
 }
