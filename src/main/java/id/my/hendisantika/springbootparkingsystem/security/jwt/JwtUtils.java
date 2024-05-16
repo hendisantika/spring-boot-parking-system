f084d3d8879b1275625d3cc6d33e49977b821e88
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:29:28 +0700

    Add JwtUtils 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/security/jwt/JwtUtils.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.springbootparkingsystem.security.jwt;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Component
+public class JwtUtils {
+
+    @Value("${deep.app.jwtSecret}")
+    private String jwtSecret;
+
+    @Value("${deep.app.jwtExpirationMs}")
+    private int jwtExpirationMs;
+
+    @Value("${deep.app.jwtCookieName}")
+    private String jwtCookie;
+}
