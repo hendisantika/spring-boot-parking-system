04fc233f1ae1b9068307ec09ce8d650f4e26a59d
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:31:27 +0700

    Add EmailService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/EmailService.java
@@ -0,0 +1,33 @@
+package id.my.hendisantika.springbootparkingsystem.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:31
+ * To change this template use File | Settings | File Templates.
+ */
+@Service("emailService")
+public class EmailService {
+
+    private final JavaMailSender javaMailSender;
+
+    @Autowired
+    public EmailService(JavaMailSender javaMailSender) {
+        this.javaMailSender = javaMailSender;
+    }
+
+    @Async
+    public void sendEmail(SimpleMailMessage email) {
+        javaMailSender.send(email);
+    }
+}
