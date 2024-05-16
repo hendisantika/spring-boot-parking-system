e842575d6d512800ee6938ef24960ebd637c90d8
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:39:45 +0700

    Add findVehicleByRegistrationNumber in VehicleServiceImpl 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/service/VehicleServiceImpl.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/VehicleServiceImpl.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 /**
  * Created by IntelliJ IDEA.
@@ -43,5 +44,12 @@ public class VehicleServiceImpl implements VehicleService {
         }
 
         return ResponseEntity.ok(allVehicles);
+    }
+
+    @Override
+    public ResponseEntity<?> findVehicleByRegistrationNumber(String registerationNumber) {
+        Optional<Vehicle> vehicle = vehicleRepository.findByRegisterationNumber(registerationNumber);
+
+        return ResponseEntity.ok(vehicle);
     }
 }
