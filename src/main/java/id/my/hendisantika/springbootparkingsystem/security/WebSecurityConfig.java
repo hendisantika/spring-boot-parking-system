417d9f4c400e831e52856f3b9555eeb77ce379f5
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:24:15 +0700

    Add authenticationManager bean in WebSecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/security/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/security/WebSecurityConfig.java
@@ -3,7 +3,9 @@ package id.my.hendisantika.springbootparkingsystem.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
@@ -53,5 +55,8 @@ public DaoAuthenticationProvider authenticationProvider() {
 //  public AuthenticationManager authenticationManagerBean() throws Exception {
 //    return super.authenticationManagerBean();
 //  }
-
+@Bean
+public AuthenticationManager authenticationManager(AuthenticationConfiguration authConfig) throws Exception {
+    return authConfig.getAuthenticationManager();
+}
 }
