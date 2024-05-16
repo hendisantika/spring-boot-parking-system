23c9e3df069754a2a0cb036d271b571c1ad9da36
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:13:07 +0700

    Add Role entity 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/entity/Role.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.springbootparkingsystem.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:12
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Enumerated(EnumType.STRING)
+    @Column(length = 20)
+    private ERole name;
+
+    public Role(ERole name) {
+        this.name = name;
+    }
+}
