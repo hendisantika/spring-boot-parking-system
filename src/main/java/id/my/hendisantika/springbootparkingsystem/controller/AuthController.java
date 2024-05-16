4c5559e793d1b775ac7ad4810065b50742bb84c4
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:48:37 +0700

    Add getUserDetails in AuthController🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/controller/AuthController.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/controller/AuthController.java
@@ -12,6 +12,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -57,5 +59,10 @@ public class AuthController {
     @PostMapping("/signout")
     public ResponseEntity<?> logoutUser() {
         return userService.logoutUser();
+    }
+
+    @GetMapping("/details/{username}")
+    public ResponseEntity<?> getUserDetails(@PathVariable("username") String username) {
+        return userService.getUserDetails(username);
     }
 }
