74f7ce92096ceb714cd967b8830622af1b360f4f
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:14:02 +0700

    Add ESize enum 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/entity/ESize.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springbootparkingsystem.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:13
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ESize {
+    SIZE_SMALL,
+    SIZE_MEDIUM,
+    SIZE_LARGE
+}
