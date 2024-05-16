a7f8590aa4f24f02a084ce61c7d5177dfd878e0b
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:35:11 +0700

    Add build in UserDetailsImpl 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserDetailsImpl.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserDetailsImpl.java
@@ -1,13 +1,17 @@
 package id.my.hendisantika.springbootparkingsystem.service;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import id.my.hendisantika.springbootparkingsystem.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by IntelliJ IDEA.
@@ -39,4 +43,19 @@ public class UserDetailsImpl implements UserDetails {
     private String drivingLicense;
 
     private Collection<? extends GrantedAuthority> authorities;
+
+    public static UserDetailsImpl build(User user) {
+        List<GrantedAuthority> authorities = user.getRoles().stream()
+                .map(role -> new SimpleGrantedAuthority(role.getName().name()))
+                .collect(Collectors.toList());
+
+        return new UserDetailsImpl(
+                user.getId(),
+                user.getUsername(),
+                user.getEmail(),
+                user.getPassword(),
+                user.getMobile(),
+                user.getDrivingLisence(),
+                authorities);
+    }
 }
