0b65ac3f6a463fe99104073c000c9a94e196d8ae
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:13:35 +0700

    Add ERole enum 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/entity/ERole.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springbootparkingsystem.entity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:13
+ * To change this template use File | Settings | File Templates.
+ */
+public enum ERole {
+    ROLE_USER,
+    ROLE_MODERATOR,
+    ROLE_ADMIN
+}
