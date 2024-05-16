0cb6b1a9400a9d1743c74af1e5059662298daecc
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:34:49 +0700

    Add UserDetailsImpl 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserDetailsImpl.java
@@ -0,0 +1,42 @@
+package id.my.hendisantika.springbootparkingsystem.service;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:34
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDetailsImpl implements UserDetails {
+    private static final long serialVersionUID = 1L;
+
+    private Long id;
+
+    private String username;
+
+    private String email;
+
+    @JsonIgnore
+    private String password;
+
+    private String mobile;
+
+    private String drivingLicense;
+
+    private Collection<? extends GrantedAuthority> authorities;
+}
