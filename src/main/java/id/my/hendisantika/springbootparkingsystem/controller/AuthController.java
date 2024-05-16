79ff395e6808ad01eb61ea6239afe2e1ae22b844
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:47:45 +0700

    Add AuthController🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/controller/AuthController.java
@@ -0,0 +1,40 @@
+package id.my.hendisantika.springbootparkingsystem.controller;
+
+import id.my.hendisantika.springbootparkingsystem.repository.RoleRepository;
+import id.my.hendisantika.springbootparkingsystem.repository.UserRepository;
+import id.my.hendisantika.springbootparkingsystem.security.jwt.JwtUtils;
+import id.my.hendisantika.springbootparkingsystem.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:47
+ * To change this template use File | Settings | File Templates.
+ */
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+    private final AuthenticationManager authenticationManager;
+
+    private final UserRepository userRepository;
+
+    private final RoleRepository roleRepository;
+
+    private final PasswordEncoder encoder;
+
+    private final JwtUtils jwtUtils;
+
+    private final UserService userService;
+}
