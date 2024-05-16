1b17abf7635a541e76aa5f61cecac5fbefb766a7
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:24:04 +0700

    Add authenticationProvider bean in WebSecurityConfig 🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/springbootparkingsystem/security/WebSecurityConfig.java
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/security/WebSecurityConfig.java
@@ -3,6 +3,7 @@ package id.my.hendisantika.springbootparkingsystem.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 /**
@@ -36,6 +37,21 @@ public class WebSecurityConfig { // extends WebSecurityConfigurerAdapter {
 //  @Override
 //  public void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
 //    authenticationManagerBuilder.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
+//  }
+@Bean
+public DaoAuthenticationProvider authenticationProvider() {
+    DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+
+    authProvider.setUserDetailsService(userDetailsService);
+    authProvider.setPasswordEncoder(passwordEncoder());
+
+    return authProvider;
+}
+
+//  @Bean
+//  @Override
+//  public AuthenticationManager authenticationManagerBean() throws Exception {
+//    return super.authenticationManagerBean();
 //  }
 
 }
