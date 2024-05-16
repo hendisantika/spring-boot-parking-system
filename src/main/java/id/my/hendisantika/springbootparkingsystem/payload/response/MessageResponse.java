865f025d8b3d01ed20dbe75b660e179c8398fd89
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:42:12 +0700

    Add MessageResponse🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/payload/response/MessageResponse.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.springbootparkingsystem.payload.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:42
+ * To change this template use File | Settings | File Templates.
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MessageResponse {
+    private String message;
+}
