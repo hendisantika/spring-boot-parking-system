42a44a137c663d0f5780871067e743d7b6df88c8
hendisantika <hendisantika@yahoo.co.id>
2024-05-17 06:36:22 +0700

    Add UserService 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootparkingsystem/service/UserService.java
@@ -0,0 +1,25 @@
+package id.my.hendisantika.springbootparkingsystem.service;
+
+import org.springframework.http.ResponseEntity;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-parking-system
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 5/17/24
+ * Time: 06:36
+ * To change this template use File | Settings | File Templates.
+ */
+public interface UserService {
+    ResponseEntity<?> authenticateUser(LoginRequest loginRequest);
+
+    ResponseEntity<?> registerUser(SignupRequest signUpRequest);
+
+    ResponseEntity<?> logoutUser();
+
+    ResponseEntity<?> getUserDetails(String username);
+
+    ResponseEntity<?> confirmEmail(String confirmationToken);
+}
